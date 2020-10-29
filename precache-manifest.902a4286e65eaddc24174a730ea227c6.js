94bb520232d97130b02cc563b92e26aa7b56efe1
aaliapkin <aaliapkin@gmail.com>
2020-10-29 19:37:04 +0300

    Updates

---
+++ precache-manifest.902a4286e65eaddc24174a730ea227c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1983d7d25d0c8a73795235beeee57ae",
+    "url": "/to-do_app/index.html"
+  },
+  {
+    "revision": "1d9cf54293ea4e345264",
+    "url": "/to-do_app/static/css/main.236672de.chunk.css"
+  },
+  {
+    "revision": "fd087f53a4d7fdda9981",
+    "url": "/to-do_app/static/js/2.e68084f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do_app/static/js/2.e68084f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d9cf54293ea4e345264",
+    "url": "/to-do_app/static/js/main.b1bf16e0.chunk.js"
+  },
+  {
+    "revision": "73313bbcdc96365a9fc8",
+    "url": "/to-do_app/static/js/runtime-main.fc759d40.js"
+  }
+]);
